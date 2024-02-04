d5695ae1ba7397a6cb041e96b285c400edc541ad
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2024-02-04 18:28:00 -0500

    json added

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const path = require("path");
-
+const db = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Get route for HTML notes
+// Get route for HTML index
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 );
 
-//Get route for index html 
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+//Get route for notes html 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 );
 
 //Get Route for API notes
 app.get('/api/notes', (req, res) => {
 
-
+res.json(db)
  });
 
 //Post route for  APIT notes
