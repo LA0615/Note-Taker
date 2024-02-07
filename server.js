2f525e7589f24e15b7159494437754ce7101b044
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2024-02-07 17:58:17 -0500

    delete post addition

--- server.js
+++ server.js
@@ -35,8 +35,31 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-//Delete requst for api notes---extra credit portion
+// DELETE a note by ID
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  
+  // Read the JSON file
+  const notes = require("./db/db.json");
+
+  // Find the index of the note with the given ID
+  const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+  // If the note is found, remove it from the array
+  if (noteIndex !== -1) {
+    notes.splice(noteIndex, 1);
+
+    // Write the updated notes array back to the JSON file
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+
+    res.json(notes);
+   } else {
+    res.status(404).json({ error: "Note not found." });
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+
